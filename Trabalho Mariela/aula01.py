b5eb3a6b4074b84fadca5e8270d3412e08a9a85c
mykaelle1109 <mykaelle1109@gmail.com>
2024-12-02 03:25:48 -0400

    Limpeza de arquivos e estruturação do código

--- Trabalho Mariela/aula01.py
+++ Trabalho Mariela/aula01.py
@@ -1,9 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash
+app_Mykaelle = Flask (__name__)
 
-app_Mariela = Flask (__name__)
+app_Mykaelle.secret_key = "secretkey"  # Necessário para mensagens flash
 
-@app_Mariela.route('/')
-def raiz():
-    return 'Olá, turma!'
+@app_Mykaelle.route("/")
+def login():
+    return render_template("login.html")
 
-app_Mariela.run()
+@app_Mykaelle.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        name = request.form["name"]
+        cpf = request.form["cpf"]
+        email = request.form["email"]
+        phone = request.form["phone"]
+        address = request.form["address"]
+        password = request.form["password"]
+        confirm_password = request.form["confirm_password"]
+
+        if password != confirm_password:
+            flash("As senhas não coincidem. Tente novamente!", "error")
+            return redirect(url_for("register"))
+        
+        flash("Cadastro realizado com sucesso!", "success")
+        return redirect(url_for("login"))
+
+    return render_template("register.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
